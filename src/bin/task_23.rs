0e6b268aa9d85715c2b56d8f6a172d6e8a44e6ff
VYAR <vadim.d.yarovoy@yandex.ru>
2024-09-07 18:56:07 +0300

    feat: Complete task 23

---
+++ src/bin/task_23.rs
@@ -0,0 +1,34 @@
+fn main() {
+    // Создаем точки
+    let point1 = geometry::Point::new(3.0, 4.0);
+    let point2 = geometry::Point::new(0.0, 0.0);
+
+    // Вычисляем расстояние
+    let distance = point1.distance(&point2);
+
+    // Результат
+    println!("Distance: {}", distance);
+}
+
+// Инкапсуляция за счет модуля
+mod geometry {
+    // Структура публичная
+    #[derive(Debug)]
+    pub struct Point {
+        // Поля приватные
+        x: f64,
+        y: f64,
+    }
+
+    impl Point {
+        // Конструктор для создания новой точки
+        pub fn new(x: f64, y: f64) -> Self {
+            Point { x, y }
+        }
+
+        // Метод для вычисления расстояния
+        pub fn distance(&self, other: &Point) -> f64 {
+            ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+        }
+    }
+}
